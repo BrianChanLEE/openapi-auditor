07cf9d8b7ac32def13a88c8212bf4150e6765764
brian_Chan_Lee <brianlee1914@me.com>
2026-02-17 20:54:24 +0900

    chore: initial release cleanup and documentation setup

--- dist/reporter/markdown-reporter.d.ts
+++
@@ -1,7 +0,0 @@
-import { AnalysisResult } from '../analyzer/result-analyzer';
-import { Priority } from '../analyzer/priority-engine';
-export declare class MarkdownReporter {
-    static generate(results: (AnalysisResult & {
-        priority: Priority;
-    })[], outputDir: string): string;
-}
--- dist/runner/test-suite.d.ts
+++
@@ -1,7 +0,0 @@
-import { ApiEndpoint } from '../openapi/loader';
-import { ApiRunner, TestResult } from './api-runner';
-export declare class TestSuite {
-    private runner;
-    constructor(runner: ApiRunner);
-    runAll(endpoints: ApiEndpoint[]): Promise<TestResult[]>;
-}
